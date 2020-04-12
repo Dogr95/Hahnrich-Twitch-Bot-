a2a8ef8096d8a627f55cc78422bfd583762e8dea
Dogr95 <39246384+Dogr95@users.noreply.github.com>
2020-04-12 22:30:24 +0200

    Update ser.py

--- ser.py
+++ ser.py
@@ -4,7 +4,7 @@ marriage = False
 
 name = input("name: ")
 uid = int(input("id: "))
-balance = input("balance: ")
+#balance = int(input("balance: "))
 p = input("partner: ")
 if p=='n':
     partner = None
@@ -16,10 +16,6 @@ data = {'name': name,
         'balance': balance,
         'partner': partner
         }
-
-#if partner != None:
-#    partner = input("partner: ")
-#    data.update({'partner': partner})
 
 with open("ser.json", "r") as f:
     ser = json.load(f)
@@ -61,91 +57,3 @@ finally:
         o = json.dumps(ser, indent=2)
         f.write(o)
     print(f"You got {Balance} credits")
-
-
-#if name in entry:
-#    pass
-#else:
-#    with open("serT.json", "w") as f:
-#       entry.append(data)    
-#       o = json.dumps(ser, indent=2)
-#       f.write(o)
-
-
-
-#with open("ser.json", "r") as f:
-#    users = json.load(f)
-#if name in users:
-#    print("True")
-#else:
-#    users.update([ ('name', name) , ('id', uid) , ('balance', balance) , ('marriage', marriage)])
-#    print(users)
-#with open("serT.json", "w") as f:
-#    output = json.dumps(users, indent=2)
-#    f.write(output)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#name = input("name: ")
-#idnumber = input("id: ")
-#creditsV = input("credits: ")
-#marriage = input("marriage: ")
-#
-#class User:
-#    def __init__(self, uid, name, balance, marriage):
-#        self.uid = uid
-#        self.name = name
-#        self.balance = balance
-#        self.marriage_status = marriage['status']
-#        self.marriage_name = marriage['name']
-#
-#    @classmethod
-#    def from_json(cls, json_string):
-#        json_dict = json.loads(json_string)
-#        return cls(**json_dict)
-#    
-#    def __repr__(self):
-#        return f'<User { self.first_name }>'
-
-
-#userdata = {
-#        "user": {
-#            "name": name,
-#            "id": idnumber,
-#            "credits": creditsV,
-#            "marriage": marriage
-#        }
-#}
-#users_list = []
-#with open("users/data.json", "r") as f:
-#    user_data = json.loads(f.read())
-#    for u in user_data:
-#        users_list.append(User(**u))
-#
-#print(users_list)
-
-#with open("users/data.json", "w") as f:
-#    json.dump(userdata, f, indent=4)
-#
-#json_str = json.dumps(userdata, indent=4)
-#print(json_str)
