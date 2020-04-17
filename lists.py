fdf2bd69013d0905d9a558b1a93c9646fd50c25f
Dogr95 <39246384+Dogr95@users.noreply.github.com>
2020-04-17 21:38:41 +0200

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,16 @@
+greetAnswer = ['Hi! selphyHi', 'Auch hier? selphyHi', 'Lang nicht gesehen selphyHi', 'Hey, was geht? selphyHi',
+               'Tag, haben wir uns schonmal gesehen? selphyHi']
+greetList = ['vertik1sachse', 'vertik1geist', 'vertik1flex', 'snaqblank', 'joelucutz', 'joelucutz,', 'hello', 'hello,',
+             'hi', 'hi,', 'tag', 'tag,', 'hallo', 'hallo,', 'selphyhi', 'selphyhi,', 'moin', 'moin,', 'servus',
+             'servus,', 'grüße', 'grüße,', 'hey', 'hey,', 'sup', 'sup,', 'hay', 'hay,' 'hoi', 'hoi,']
+greetWList = ['wie gehts', 'was geht', 'und dir?', 'deine lage?', 'bei dir?']
+greetWAnswer = ['Ganz okay... hoffentlich bald Feierabend selphySweat', 'Mir gehts ganz gut und selber? selphySmug',
+                'Das geht dich nichts an! selphyPout', 'Meine Lage ist unbestimmt seit der Zeit in Vietnam selphySad',
+                'Sag du es mir selphyIQ', 'Die Frage ist, wie gehts DIR? selphyAra',
+                'Lass dir mal ne andere Frage einfallen selphyRage']
+husoList = ['huso', 'haʟʟo', 'haʟʟo,', 'haiio', 'hailo', 'hailo,', 'halio', 'halio,', 'haiio,', 'halo', 'halo,',
+            'was los klaus', 'alles husos', 'husos', 'erschieß dich', 'erschiess dich',
+            'account vor drei minuten erstellt', 'xhuso']
+husoAnswer = ['Alles Husos, was los Klaus.', 'Account vor drei Minuten erstellt, ahja', 'ahja', 'xhuso']
+ripList = ['rip', 'rip,', 'f', 'f,', 'selphysad', 'noo', 'nooo', 'noooo']
+ripAnswer = ['F', 'NOOO', 'WHY', 'warum tust du das?', 'alles husos', 'beim nächsten mal läufts bestimmt besser']
