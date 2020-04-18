8e322043f531744f5344763012adc242e1d7e863
Dogr95 <39246384+Dogr95@users.noreply.github.com>
2020-04-18 20:53:23 +0200

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,6 +6,7 @@ from discord.ext import commands
 import time
 import json
 import satzgenerator
+from lists import *
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -70,6 +71,17 @@ async def on_member_join(member):
     newMember = (f"{member.name}")
     await owner.send(newMember)
 
+@bot.event
+async def on_member_update(before, after):
+    n = after.nick
+    if n: # Check if they updated their username
+        if n.lower().count("ey") > 0: # If username contains tim
+        else:
+            last = before.nick
+            if last: # If they had a username before change it back to that
+                await after.edit(nick=last)
+            else: # Otherwise set it to "NO STOP THAT"
+                await after.edit(nick="NO STOP THAT")
 
 # @bot.event
 # async def on_message(message, mCont):
@@ -577,22 +589,6 @@ async def lotto(ctx, *lottoG):
 
 @bot.command(name='pomf', help="even the bot doesn't know what this does")
 async def pomf(ctx):
-    greetAnswer = ['Hi! selphyHi', 'Auch hier? selphyHi', 'Lang nicht gesehen selphyHi', 'Hey, was geht? selphyHi',
-                   'Tag, haben wir uns schonmal gesehen? selphyHi']
-    greetList = ['vertik1sachse', 'vertik1geist', 'vertik1flex', 'snaqblank', 'joelucutz', 'joelucutz,', 'hello',
-                 'hello,', 'hi', 'hi,', 'tag', 'tag,', 'hallo', 'hallo,', 'selphyhi', 'selphyhi,', 'moin', 'moin,',
-                 'servus', 'servus,', 'grüße', 'grüße,', 'hey', 'hey,', 'sup', 'sup,', 'hay', 'hay,' 'hoi', 'hoi,']
-    greetWList = ['wie gehts', 'was geht', 'und dir?', 'deine lage?', 'bei dir?']
-    greetWAnswer = ['Ganz okay... hoffentlich bald Feierabend selphySweat', 'Mir gehts ganz gut und selber? selphySmug',
-                    'Das geht dich nichts an! selphyPout',
-                    'Meine Lage ist unbestimmt seit der Zeit in Vietnam selphySad', 'Sag du es mir selphyIQ',
-                    'Die Frage ist, wie gehts DIR? selphyAra', 'Lass dir mal ne andere Frage einfallen selphyRage']
-    husoList = ['huso', 'haʟʟo', 'haʟʟo,', 'haiio', 'hailo', 'hailo,', 'halio', 'halio,', 'haiio,', 'halo', 'halo,',
-                'was los klaus', 'alles husos', 'husos', 'erschieß dich', 'erschiess dich',
-                'account vor drei minuten erstellt', 'xhuso']
-    husoAnswer = ['Alles Husos, was los Klaus.', 'Account vor drei Minuten erstellt, ahja', 'ahja', 'xhuso']
-    ripList = ['rip', 'rip,', 'f', 'f,', 'selphysad', 'noo', 'nooo', 'noooo']
-    ripAnswer = ['F', 'NOOO', 'WHY', 'warum tust du das?', 'alles husos', 'beim nächsten mal läufts bestimmt besser']
     liste = random.choice([greetAnswer, greetWAnswer, husoAnswer, ripAnswer])
     listeSmol = random.choice(liste)
     listeMessage = listeSmol + f" {ctx.author.name} selphyHae"
